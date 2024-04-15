461f7dbc01729adcb462d760ca87fb52173a104e
Seongbin <bins506@gmail.com>
2024-04-15 16:41:08 +0900

    seminar_4

---
+++ pard_3rd_seminar_4/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MyInfo from "./Pages/MyInfo";
+import {Route, Routes} from "react-router-dom";
+import UpdateInfo from "./Pages/UpdateInfo";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<MyInfo />} />
+      <Route path="/update" element={<UpdateInfo />} />
+    </Routes>
+  );
+}
+
+export default App;
